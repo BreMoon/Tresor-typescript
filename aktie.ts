140b5a4c035eec023ba2d40956c6f60b7e776e9c
BreMoon <79579221+BreMoon@users.noreply.github.com>
2023-01-19 11:00:35 +0100

    2 files modified

--- aktie.ts
+++ aktie.ts
@@ -1,7 +1,18 @@
-import {Gegenstand} from "./gegenstand";
+import { Gegenstand } from './gegenstand';
 
 export class aktie extends Gegenstand {
-  
-  constructor (id: number, wert; )
+  constructor(
+    public id: number,
+    public wert: number,
+    public unternehmen: String,
+    public nennwert: number
+  ) {
+    super(id, wert);
+  }
 
+  toString(): string {
+    let text: string = '\nUnternehmen: ' + this.unternehmen;
+    text += super.toString();
+    return text;
+  }
 }
--- schmuck.ts
+++ schmuck.ts
@@ -1,9 +1,16 @@
 import { Gegenstand } from './gegenstand'; //von Stackblitz automat. generiert
 
 export class Schmuck extends Gegenstand {
-  constructor (public id: number,public wert: number, public bezeichnung: String){
-    super(id, wert)
+  constructor(
+    public id: number,
+    public wert: number,
+    public bezeichnung: String
+  ) {
+    super(id, wert);
+  }
+  toString(): string {
+    let text: string = '\nBezeichnung: ' + this.bezeichnung;
+    text += super.toString();
+    return text;
   }
 }
-
-//todo
